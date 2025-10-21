b34f747aef1abf424b50d973a8bb0d505699e89d
Louis Kaiser <louis.kaiser0203@icloud.com>
2025-10-21 22:05:11 +0200

    Initial Commit

---
+++ contactSync/contactSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactSyncApp.swift
+//  contactSync
+//
+//  Created by Louis Kaiser on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct contactSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
